4a3b5f9acee7dacab7ad7ff017904daeaa2c98a7
hasannayeem71 <2052cse00867@manarat.ac.bd>
2021-09-01 17:21:31 +0600

    added spinner and error messege

--- js/app.js
+++ js/app.js
@@ -1,27 +1,48 @@
 // load data from api
-const loadData = (cityName) =>{
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fd75739370d96ca45ebf327a5581be52`)
-    .then(res=>res.json())
-    .then(data=>viewData(data));
-}
+const loadData = (cityName = "gazipur") => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fd75739370d96ca45ebf327a5581be52`
+  )
+    .then((res) => res.json())
+    .then((data) => viewData(data));
+};
 
 // view necessary data from api
-const viewData = data=>{
-    const display = document.getElementById('display');
-    display.innerHTML=`
+const viewData = (data) => {
+  console.log();
+  if (data.cod == 200) {
+    const display = document.getElementById("display");
+    display.innerHTML = `
             <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="">
-            <h1>${ data.name}</h1>
-            <h3><span>${(data.main.temp/10.0).toFixed(2)}</span>&deg;C</h3>
-            <h1 class="lead">${data.weather[0].main}</h1>
-    `
-}
+            <h1>${data?.name}</h1>
+            <h3><span>${(data?.main?.temp / 10.0).toFixed(2)}</span>&deg;C</h3>
+            <h1 class="lead">${data?.weather[0].main}</h1>
+    `;
+    spinnerToggle("none");
+  } else {
+    errorToggle("block");
+    spinnerToggle("none");
+  }
+};
 // deafult call
-loadData('Gazipur');
+loadData();
 
 // searching call
-const viewInputData = () =>{
-const inputField = document.getElementById('inputbox');
-const inputText = inputField.value;
-inputField.value='';
-loadData(inputText)
-}
+const viewInputData = () => {
+  errorToggle("none");
+
+  const inputField = document.getElementById("inputbox");
+  const inputText = inputField.value;
+  inputField.value = "";
+  loadData(inputText);
+  spinnerToggle("block");
+};
+
+//spinner
+const spinnerToggle = (displayStyle) => {
+  document.getElementById("spinner").style.display = displayStyle;
+};
+// error toggle
+const errorToggle = (displayStyle) => {
+  document.getElementById("error").style.display = displayStyle;
+};
